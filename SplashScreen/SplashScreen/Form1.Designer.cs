b302e7dbe5b407ded20e1d083216c416ed94c466
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2023-03-19 18:51:39 +0200

    Complete Splash Screen Form

--- SplashScreen/SplashScreen/Form1.Designer.cs
+++ SplashScreen/SplashScreen/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            progressbar1 = new CircularProgressBar.CircularProgressBar();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // progressbar1
+            // 
+            progressbar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            progressbar1.AnimationSpeed = 500;
+            progressbar1.BackColor = Color.FromArgb(42, 40, 60);
+            progressbar1.Font = new Font("Comic Sans MS", 39.75F, FontStyle.Bold, GraphicsUnit.Point);
+            progressbar1.ForeColor = Color.FromArgb(85, 213, 219);
+            progressbar1.InnerColor = Color.FromArgb(42, 40, 60);
+            progressbar1.InnerMargin = 2;
+            progressbar1.InnerWidth = -1;
+            progressbar1.Location = new Point(50, 98);
+            progressbar1.MarqueeAnimationSpeed = 2000;
+            progressbar1.Name = "progressbar1";
+            progressbar1.OuterColor = Color.FromArgb(26, 28, 43);
+            progressbar1.OuterMargin = -25;
+            progressbar1.OuterWidth = 26;
+            progressbar1.ProgressColor = Color.FromArgb(85, 213, 219);
+            progressbar1.ProgressWidth = 6;
+            progressbar1.SecondaryFont = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            progressbar1.Size = new Size(150, 150);
+            progressbar1.StartAngle = 270;
+            progressbar1.Style = ProgressBarStyle.Continuous;
+            progressbar1.SubscriptColor = Color.FromArgb(166, 166, 166);
+            progressbar1.SubscriptMargin = new Padding(10, -35, 0, 0);
+            progressbar1.SubscriptText = "";
+            progressbar1.SuperscriptColor = Color.FromArgb(166, 166, 166);
+            progressbar1.SuperscriptMargin = new Padding(10, 35, 0, 0);
+            progressbar1.SuperscriptText = "";
+            progressbar1.TabIndex = 0;
+            progressbar1.TextMargin = new Padding(8, 8, 0, 0);
+            progressbar1.Value = 68;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 24.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(85, 213, 219);
+            label1.Location = new Point(75, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 45);
+            label1.TabIndex = 1;
+            label1.Text = "Text";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Comic Sans MS", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(85, 213, 219);
+            label2.Location = new Point(75, 308);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 29);
+            label2.TabIndex = 1;
+            label2.Text = "Loading...";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Comic Sans MS", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(85, 213, 219);
+            label3.Location = new Point(13, 351);
+            label3.Name = "label3";
+            label3.Size = new Size(230, 18);
+            label3.TabIndex = 1;
+            label3.Text = "Copyright © 2023 Christos Kontorias";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(42, 40, 60);
+            ClientSize = new Size(255, 378);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(progressbar1);
+            Cursor = Cursors.WaitCursor;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Opacity = 0.9D;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private CircularProgressBar.CircularProgressBar progressbar1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- SplashScreen/SplashScreen/Form1.cs
+++ SplashScreen/SplashScreen/Form1.cs
@@ -1,10 +1,43 @@
-namespace SplashScreen
+using System.Drawing.Drawing2D;
+
+namespace SplashScreen;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
+        InitializeComponent();
+        // Set the size and position of the form
+
+        // Create a GraphicsPath that defines the rounded rectangle shape
+        GraphicsPath path = new GraphicsPath();
+        int arcSize = 20; // adjust this value to control the roundness of the corners
+        path.AddArc(0, 0, arcSize, arcSize, 180, 90); // top-left corner
+        path.AddArc(this.Width - arcSize, 0, arcSize, arcSize, 270, 90); // top-right corner
+        path.AddArc(this.Width - arcSize, this.Height - arcSize, arcSize, arcSize, 0, 90); // bottom-right corner
+        path.AddArc(0, this.Height - arcSize, arcSize, arcSize, 90, 90); // bottom-left corner
+        path.CloseAllFigures();
+
+        // Set the Region property of the form to the custom GraphicsPath
+        this.Region = new Region(path);
+        progressbar1.Value = 0;
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+        progressbar1.Value += 1;
+        progressbar1.Text = progressbar1.Value.ToString() + "%";
+        if (progressbar1.Value == 100)
         {
-            InitializeComponent();
+            timer1.Enabled = false;
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
         }
     }
 }
